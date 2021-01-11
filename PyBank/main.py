59f99784d15c72ce4c9760ce4cc8a2f820be5c25
Efrain Lopez <e.lopez515@outlook.com>
2021-01-10 20:20:37 -0800

    I looped through the data to git commit -m

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,6 +18,10 @@ with open(budgets_csv, 'r') as csv_file:
     #Define our variables
     total_months = 0
     net_total_profit_losses = 0
+    net_change_profit_losses = 0
+    previous_profit_losses = 0
+    profit_losses_change = 0
+    profit_losses_change_count = 0    
 
     #Loop through the data in our csvfile
     for row in csv_reader:
@@ -26,10 +30,23 @@ with open(budgets_csv, 'r') as csv_file:
         current_profit_losses = int(row[1])
         net_total_profit_losses += current_profit_losses
 
-
        #Calculate the total amount of months
         total_months += 1
 
+
+        #Loop through the data to calculate for monthly change of profits/losses..
+        #... and the total amount of monthly changes in profit_losses_change_count
+        if previous_profit_losses != 0:
+            profit_losses_change = current_profit_losses - previous_profit_losses
+            net_change_profit_losses += profit_losses_change
+            profit_losses_change_count += 1
+
+        #Reset the variable so that the next month is subtracted by its preceding month
+        previous_profit_losses = current_profit_losses 
+
+    #calculate the average change using values found in the for previous loop
+    average_change = net_change_profit_losses/profit_losses_change_count
+
+    print(average_change)
     print(net_total_profit_losses)
-    #print(profit_losses_change)
     print(total_months)
