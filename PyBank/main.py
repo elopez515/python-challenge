230c837b4ab5c1193998ce9273502ee208f2b495
Efrain Lopez <e.lopez515@outlook.com>
2021-01-10 19:22:43 -0800

    Found the total months and calculated for total net profit/losses

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,40 +5,31 @@ import csv
 budgets_csv = os.path.join("Resources", "budget_data.csv")
 
 #read in the csv file
-with open(budgets_csv, 'r') as csvfile:
+with open(budgets_csv, 'r') as csv_file:
 
     # Split the data on commas
-    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_reader = csv.reader(csv_file, delimiter=',')
 
     
     #Read the header row first
-    csv_header = next(csvfile)
+    csv_header = next(csv_file)
     print(f"Header: {csv_header}")
 
+    #Define our variables
+    total_months = 0
+    net_total_profit_losses = 0
+
     #Loop through the data in our csvfile
-    for row in csvfile:
+    for row in csv_reader:
 
-         print(row)
-         exit()
-        
-       
+        #calculate for the net total of profit/losses
+        current_profit_losses = int(row[1])
+        net_total_profit_losses += current_profit_losses
 
-# #Define the functions and accept 'budget_data' as the sole parameter
-# def budgets_data(row):
-#     months = str(row[0])
-#     profit_losses = int(row[1])
-   
-#    #calculate the total number of months in our data set
-#      total_months = len(months)
-#     print(total_months)
-#     print(budgets_data)
-# #Define the function to calculate average profit/loss
-# def average(profit_losses): 
-#     length = len(profit_losses)
-#     total = 0.0
-# for profit_loss in profit_losses:
 
-# return total / length
+       #Calculate the total amount of months
+        total_months += 1
 
-#     #Calculate the average profit/loss
-#     print(average(profit_losses))
+    print(net_total_profit_losses)
+    #print(profit_losses_change)
+    print(total_months)
